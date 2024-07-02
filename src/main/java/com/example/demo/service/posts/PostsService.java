548ecead7ababaa33ea794cd4d14da89f1eb71df
suhyeoonn <suhyeon1740@gmail.com>
2024-07-02 22:30:53 +0900

    조회화면 구현

--- src/main/java/com/example/demo/service/posts/PostsService.java
+++ src/main/java/com/example/demo/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.example.demo.service.posts;
 
 import com.example.demo.domain.posts.Posts;
 import com.example.demo.domain.posts.PostsRepository;
+import com.example.demo.web.dto.PostsListResponseDto;
 import com.example.demo.web.dto.PostsResponseDto;
 import com.example.demo.web.dto.PostsSaveRequestDto;
 import com.example.demo.web.dto.PostsUpdateRequestDto;
-import jakarta.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -31,4 +35,13 @@ public class PostsService {
         Posts entity = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id ="+id));
         return new PostsResponseDto(entity);
     }
+
+    // readOnly = true: 트랜잭션 범위는 유지하되 조회 기능만 남겨두어 조회 속도가 개선됨
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDec() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
 }
